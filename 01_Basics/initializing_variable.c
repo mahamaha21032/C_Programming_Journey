790abcf60622c443608e97b26d8860d11b23453b
mahamaha21032 <mahamaha21032@gmail.com>
2026-07-10 11:18:14 +0530

    Adding a program to create variable

---
+++ 01_Basics/initializing_variable.c
@@ -0,0 +1,7 @@
+#include <stdio.h>      //including std i/o library
+int main()              //main function
+{
+    int a=10;       //initializing a
+    printf("%d",a);     //display a value
+    return 0;       //end of the program
+}

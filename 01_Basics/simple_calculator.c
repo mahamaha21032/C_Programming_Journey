61136fffb39b80c00f90c003dc1fc8a9b4e2359d
mahamaha21032 <mahamaha21032@gmail.com>
2026-07-17 09:58:13 +0530

    Adding a program that create the simple calculator

---
+++ 01_Basics/simple_calculator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a=5;
+    int b=2;
+    printf("add:%d\n",a+b);
+    printf("sub:%d\n",a-b);
+    printf("multi:%d\n",a*b);
+    printf("div:%d",a/b);
+    return 0;
+}
